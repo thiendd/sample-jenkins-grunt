0cac95e915f31ff583652a488e1503bbe41dc62f
Thien Duong <thien.duong@indexmedia.com>
2014-05-23 16:17:56 +0700

    add tap

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -2,3 +2,6 @@ export PATH=/usr/local/bin:/usr/local/bin/node:/usr/local/bin:/usr/bin/phantomjs
 npm install grunt-cli --save-dev
 npm install
 grunt jenkins --no-color
+
+mkdir -p reports
+make test REPORTER=tap > reports/test_results.tap
